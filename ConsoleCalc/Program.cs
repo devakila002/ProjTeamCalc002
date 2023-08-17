8c6b43269046efa097a5dd2ddd39aebedb1d5500
devakila <dev.akila001@gmail.com>
2023-08-17 18:44:52 +0530

    Updates to project

---
+++ ConsoleCalc/Program.cs
@@ -0,0 +1,6 @@
+﻿public class ConsoleCalc {
+    public static void Main(string[] args){
+        int result = Convert.ToInt32(args[0]) + Convert.ToInt32(args[1]);
+        System.Console.WriteLine("Your result is " + result.ToString());
+    }
+}
